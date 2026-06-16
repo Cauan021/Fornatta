c19dab2c240b91eb1a613c12ef692807325e6121
Cauan Rodrigues de Sousa <cauan.sousa21@aluno.ce.gov.br>
2026-06-16 11:31:44 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fornatta</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <h1>FONATTA</h1>
+        <p>Feito com carinho, entregue com sabor</p>
+
+        <div class="botoes">
+            <a href="login.php"></a>
+        </div>
+    </header>
+    
+</body>
+</html>
